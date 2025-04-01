f2aecce8956b8a765f8ba9ce61443ad1e28be317
Sterennggn <sterenn.gougeon@gmail.com>
2025-04-01 20:37:36 +0200

    test update

--- public/seasons.js
+++ public/seasons.js
@@ -10,4 +10,5 @@ footer.innerHTML += `<a href='/'>Retour au menu</a>`;
 document.body.appendChild(seasonList);
 document.body.appendChild(footer);
 
-//calendrier des courses (trouver si l'api le permet ou en trouver une autre sinon), et résultats déjà connus
+// calendrier des courses (trouver si l'api le permet ou en trouver une autre sinon), et résultats déjà connus
+// pilotes qui ont au moins gagnés un championnats et qui font partis de la saisons 2025 avec la date de leur win
